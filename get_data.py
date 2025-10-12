169766b18d9998c7afb6934449ee83f90d898aa3
Devis28 <davidteplan5@gmail.com>
2025-10-12 21:44:16 +0200

    rename: get_listeners.py -> get_data.py

---
+++ get_data.py
@@ -0,0 +1,6 @@
+import requests
+
+URL = "https://shaggy-lorna-melody-now-eea13251.koyeb.app/now"
+
+data = requests.get(URL, timeout=10, headers={"Accept":"application/json"}).json()
+print(f"{data['artist']} — {data['title']} | {data['date']} {data['time']} | poslucháči: {data['listeners']}")
--- get_listeners.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-URL = "https://shaggy-lorna-melody-now-eea13251.koyeb.app/listeners"
-
-data = requests.get(URL, timeout=10, headers={"Accept":"application/json"}).json()
-print(f"{data['listeners']}")
